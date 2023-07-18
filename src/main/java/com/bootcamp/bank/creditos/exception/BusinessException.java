07f4f9ed3cb7ba5278e1848b5eddf6f4481ef12d
edymartinhf1 <edymartinhf1@gmail.com>
2023-07-17 19:46:36 -0500

    sonar fix

--- src/main/java/com/bootcamp/bank/creditos/exception/BusinessException.java
+++ src/main/java/com/bootcamp/bank/creditos/exception/BusinessException.java
@@ -1,19 +1,11 @@
 package com.bootcamp.bank.creditos.exception;
 
 public class BusinessException extends RuntimeException{
-    private String errorCode;
-    private String messageError;
 
     public BusinessException(String messageError) {
         super(messageError);
-        this.messageError = messageError;
     }
 
-    public BusinessException(String errorCode,String messageError) {
-        super(messageError);
-        this.errorCode = errorCode;
-        this.messageError = messageError;
-    }
 
 
 }
